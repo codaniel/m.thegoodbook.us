c29f5c57fa9af9ff295eef2c73cbae4e6562d977
Chris ODaniel <chrisodan@gmail.com>
2012-04-20 17:55:05 -0400

    Removed the <head> portion of the instructions page. Also removed the google analytics and helper.js reference.

--- instructions.php
+++ instructions.php
@@ -1,20 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>User Manual - KJV Bible - thegoodbook.us</title>
-<meta name="description" content="m.thegoodbook.us is an online King James Version Bible that is optimized for mobile phones. Come and see what God's word has in store for you.">
-<meta name="HandheldFriendly" content="True">
-<meta name="MobileOptimized" content="320"/>
-<meta name="viewport" content="width=device-width">
-<meta http-equiv="cleartype" content="on">
-<link rel="stylesheet" href="//code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.css" />
-<link rel="stylesheet" href="/css/style.css">
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-<script src="//code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.js"></script>
-<script src="/js/script.js"></script>
-</head>
-<body>
 <div data-role="page" data-dom-cache="true">
 	<div data-role="header">
   	<h1>User Manual</h1>
@@ -37,14 +20,4 @@
       <li>The bar on the top of the page contains the different highlighter colors. Tap on the color you want to use. Then tap on the verse you wish to highlight. To remove a highlight tap on the black button with an x then tap on the highlight you wish to remove.</li>
     </ul>  
   </div>
-  <script src="js/mylibs/helper.js"></script>
-	<script>MBP.scaleFix();</script>
-  <script>		
-    var _gaq=[["_setAccount","UA-29096062-1"],["_trackPageview"]];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-    g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
-    s.parentNode.insertBefore(g,s)}(document,"script"));
-  </script>
 </div>
-</body>
-</html>
